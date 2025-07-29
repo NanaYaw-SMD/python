0b09ef162d93214ac6c094bd6f40a129a0d91291
NanaYaw Awusi <nanayawawusi7@gmail.com>
2025-07-29 13:30:53 +0000

    Add files via upload

---
+++ save .py
@@ -0,0 +1,16 @@
+name = "Alice"
+age = 21
+color = "blue"
+food = "pizza"
+city = "Accra"
+shs = "Holy Child"
+team = "Real Madrid"
+
+
+print(f"Hello {name}")
+print(f"You are {age} years old")
+print(f"Your favorite color is {color}")
+print(f"And your favorite food is {food}")
+print(f"Your place of residence is  {city}")
+print(f"You attend school at {shs} senior high school")
+print(f"And your favorite football club is {team}")
